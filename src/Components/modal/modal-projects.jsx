f3259aa8db920ce915eb773b0eeeadcaabff40d0
aqlhsyfqhsym <nuraqilah.syafiqah@yahoo.com>
2024-02-12 16:03:05 +0800

    portfolio projects

--- src/Components/modal/modal-projects.jsx
+++ src/Components/modal/modal-projects.jsx
@@ -2,56 +2,93 @@ import { AnimatePresence, motion } from "framer-motion";
 import { FiAlertCircle } from "react-icons/fi";
 import styles from "./modal.module.scss";
 
-const modalImage = ({ isOpen, setIsOpen, 	title, description, }) => {
-    return (
-      <AnimatePresence>
-        {isOpen && (
+const modalImage = ({
+  isOpen,
+  setIsOpen,
+  displayContentA,
+  displayContentB,
+  title,
+  description,
+  siteName,
+  livesite,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+}) => {
+  const contentA = {
+    display: displayContentA,
+  };
+
+  const contentB = {
+    display: displayContentB,
+  };
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onClick={() => setIsOpen(false)}
+          className={styles.customOverlay}
+        >
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={() => setIsOpen(false)}
-            className={styles.customOverlay}
+            initial={{ scale: 0, rotate: "12.5deg" }}
+            animate={{ scale: 1, rotate: "0deg" }}
+            exit={{ scale: 0, rotate: "0deg" }}
+            onClick={(e) => e.stopPropagation()}
+            className={styles.cardCtm}
           >
-            <motion.div
-              initial={{ scale: 0, rotate: "12.5deg" }}
-              animate={{ scale: 1, rotate: "0deg" }}
-              exit={{ scale: 0, rotate: "0deg" }}
-              onClick={(e) => e.stopPropagation()}
-              className={styles.cardCtm}
-            >
-              <FiAlertCircle className={styles.ctmText} />
-              <div className="relative z-10">
-                <div className={styles.circleContainer}>
+            {/* <FiAlertCircle className={styles.ctmText} /> */}
+            <div className="relative z-10">
+              {/* <div className={styles.circleContainer}>
                   <FiAlertCircle />
-                </div>
-                <h3 className="text-3xl font-bold text-center mb-2">
-                  {title}
-                </h3>
-                <p>
-                 {description}
-                </p>
-                <div className={styles.containerWrap}>
-                  <button
-                    onClick={() => setIsOpen(false)}
-                    className={styles.secondaryCtmButtom}
-                  >
-                    Nah, go back
-                  </button>
-                  <button
-                    onClick={() => setIsOpen(false)}
-                    className={styles.primaryCtmButton}
-                  >
-                    Understood!
-                  </button>
+                </div> */}
+              <h3 className="text-3xl font-bold text-center mb-2">
+                Project: {title}
+              </h3>
+              <div style={contentA}>
+                <div className={styles.containerCollage}>
+                  <div className={styles.web}>
+                    <img src={image1} alt="" />
+                  </div>
+                  <div className={styles.logo1}>
+                    <img src={image2} alt="" />
+                  </div>
+                  <div className={styles.logo2}>
+                    <img src={image3} alt="" />
+                  </div>
+                  <div className={styles.mobile}>
+                    <img
+                      src={image4}
+                      alt=""
+                    />
+                  </div>
                 </div>
               </div>
-            </motion.div>
+              <div style={contentB}>
+              <img src={image5} alt="" className={styles.saImage}/>
+              </div>
+              <div className={styles.containerDescText}>
+                <p>{description}</p>
+                Live site: <a href={livesite}>{siteName}</a>
+              </div>
+              <div className={styles.containerWrap}>
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className={styles.primaryCtmButton}
+                >
+                  Go back
+                </button>
+              </div>
+            </div>
           </motion.div>
-        )}
-      </AnimatePresence>
-    );
-  };
-  
-
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
 export default modalImage;
--- src/app/projects/projects.jsx
+++ src/app/projects/projects.jsx
@@ -36,8 +36,23 @@ export default function Home() {
             <Modal
               isOpen={modal.project1}
               setIsOpen={() => setModal({ project1: false })}
-              title="project 1"
-              description={<>project 1</>}
+              title="Powefy-AU"
+              description={
+                <>
+                  Develop the entire system using React.js, Next.js, and
+                  Bootstrap/SCSS, ensuring precise implementation of the design
+                  envisioned by the creative team in Figma{" "}
+                </>
+              }
+              siteName="Hot Water Saving"
+              livesite="https://hotwatersavings.au/"
+              image1="img/project/web.png"
+              image2="img/project/logo-1.png"
+              image3="img/project/hws_logo2.png"
+              image4="img/project/powerfy_mobile.gif"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
           </div>
           <div class={styles.card2}>
@@ -50,13 +65,29 @@ export default function Home() {
             <Modal
               isOpen={modal.project2}
               setIsOpen={() => setModal({ project2: false })}
-              title="project 2"
-              description={<>project 2</>}
+              title="Powefy-AU"
+              description={
+                <>
+                  Develop the entire system using React.js, Next.js, and
+                  Bootstrap/SCSS, ensuring precise implementation of the design
+                  envisioned by the creative team in Figma{" "}
+                </>
+              }
+              siteName="Hot Water Saving"
+              livesite="https://hotwatersavings.au/"
+              image1="img/project/web.png"
+              image2="img/project/logo-1.png"
+              image3="img/project/hws_logo2.png"
+              image4="img/project/powerfy_mobile.gif"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
           </div>
-          <div class={styles.card3}> 
+          <div class={styles.card3}>
             <img
-              className={styles.imageCard} style={{backgroundColor: "#fff"}} 
+              className={styles.imageCard}
+              style={{ backgroundColor: "#fff" }}
               src="img/project/golf_mobile.png"
               alt=""
               onClick={() => openModal("project3")}
@@ -64,23 +95,57 @@ export default function Home() {
             <Modal
               isOpen={modal.project3}
               setIsOpen={() => setModal({ project3: false })}
-              title="project 3"
-              description={<>project 3</>}
+              title="Golf Booking System (UI/UX)"
+              description={
+                <>
+                  It's actually an internal project for my current company. I'm
+                  responsible for designing from scratch using Adobe XD,
+                  starting with wireframes and prototypes. Research has also
+                  been conducted by my team and me. The design was inspired by
+                  the latest aesthetic trends, incorporating current-gen
+                  elements.{" "}
+                </>
+              }
+              siteName="Behance Case Study"
+              livesite="https://www.behance.net/gallery/164928797/Golf-Booking-Study-Case/modules/930160343"
+              image1="img/project/golf_1.png"
+              image2="img/project/golf_2.png"
+              image3="img/project/golf_3.png"
+              image4="img/project/golf_mobile.gif"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
           </div>
           <div class={styles.card4}>
             <img
-              className={styles.imageCard}  
+              className={styles.imageCard}
               src="img/project/boost_logo.png"
               alt=""
               onClick={() => openModal("project4")}
             />
-            
+
             <Modal
               isOpen={modal.project4}
               setIsOpen={() => setModal({ project4: false })}
-              title="project 4"
-              description={<>project 4</>}
+              title="Boost Bank"
+              description={
+                <>
+                  Collaborated with the creative team to translate designs into
+                  interactive web pages using storybook, developed dynamic UI
+                  components, ensured Drupal backend integration, and addressed
+                  design issues during SIT/UAT.{" "}
+                </>
+              }
+              siteName="Boost Bank"
+              livesite="https://newsite.myboostbank.co/"
+              image1="img/"
+              image2="img/project/boost_logo.png"
+              image3="img/project/boost_logo.png"
+              image4="img/project/boost_logo.png"
+              displayContentA="none"
+              displayContentB="block"
+              image5="img/project/boost_logo.png"
             />
           </div>
           <div class={styles.card5}>
@@ -93,14 +158,22 @@ export default function Home() {
             <Modal
               isOpen={modal.project5}
               setIsOpen={() => setModal({ project5: false })}
-              title="project 5"
-              description={<>project 5</>}
+              title="Personal Website"
+              description={<>im using pure react and pure scss hehehe </>}
+              siteName="Aqilah Syafiqah"
+              livesite="aqilahsyafiqah.com"
+              image1="img/"
+              image2="img/ "
+              image3="img/ "
+              image4="img/ "
+              displayContentA="none"
+              displayContentB="block"
+              image5="img/project/personal.gif"
             />
           </div>
-          <div class={styles.card6} >
+          <div class={styles.card6}>
             <img
-              className={styles.imageCard} 
-             
+              className={styles.imageCard}
               src="img/project/logo-1.png"
               alt=""
               onClick={() => openModal("project6")}
@@ -108,13 +181,28 @@ export default function Home() {
             <Modal
               isOpen={modal.project6}
               setIsOpen={() => setModal({ project6: false })}
-              title="project6"
-              description={<>project6</>}
+              title="Powefy-AU"
+              description={
+                <>
+                  Develop the entire system using React.js, Next.js, and
+                  Bootstrap/SCSS, ensuring precise implementation of the design
+                  envisioned by the creative team in Figma{" "}
+                </>
+              }
+              siteName="Hot Water Saving"
+              livesite="https://hotwatersavings.au/"
+              image1="img/project/web.png"
+              image2="img/project/logo-1.png"
+              image3="img/project/hws_logo2.png"
+              image4="img/project/powerfy_mobile.gif"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
           </div>
           <div class={styles.card7}>
             <img
-              className={styles.imageCard} 
+              className={styles.imageCard}
               src="img/project/baked_mobile.png"
               alt=""
               onClick={() => openModal("project7")}
@@ -122,25 +210,44 @@ export default function Home() {
             <Modal
               isOpen={modal.project7}
               setIsOpen={() => setModal({ project7: false })}
-              title="project7"
-              description={<>project7</>}
+              title="Adobe XD Certificate assesment"
+              description={<>im using pure react and pure scss hehehe </>}
+              siteName="Adobe XD Certificate"
+              livesite="https://www.udemy.com/certificate/UC-e4c07591-3953-4a86-9767-b385580d9f46/"
+              image1="img/project/certificate.png"
+              image2="img/project/baked_1.png "
+              image3="img/project/baked_2.png"
+              image4="img/project/baked2_mobile.png"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
           </div>
           <div class={styles.card8}>
             <img
               className={styles.imageCard}
-               src="img/project/task_web.gif"
+              src="img/project/task_web.gif"
               alt=""
               onClick={() => openModal("project8")}
             />
             <Modal
               isOpen={modal.project8}
               setIsOpen={() => setModal({ project8: false })}
-              title="project8"
-              description={<>project8</>}
+              title="Task Management System (UI/UX & Front-End)"
+              description={<>In House System, develop using larevel PHP, HTML/CSS as a front side with bootstrap framework. some animation are from scratch. </>}
+              siteName="Cara Task Management System"
+              livesite="https://task.cara.com.my/"
+              image1="img/project/certificate.png"
+              image2="img/project/baked_1.png "
+              image3="img/project/baked_2.png"
+              image4="img/project/baked2_mobile.png"
+              displayContentA="block"
+              displayContentB="none"
+              image5="img/"
             />
-          </div> 
-        </div> 
+        
+          </div>
+        </div>
       </div>
 
       {/* tictac section */}
